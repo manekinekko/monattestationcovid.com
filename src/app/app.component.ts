c25e5e3071604606a99ae9bac4dbb9bf4d3bc1eb
Wassim Chegham <github@wassim.dev>
2020-03-17 16:36:29 +0100

    fix: use the right day value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -264,7 +264,7 @@ export class AppComponent {
     const yearValue = date.getFullYear();
     const month = date.getMonth() + 1;
     const monthValue = (month < 10 ? `0` : ``) + month;
-    const day = date.getDay();
+    const day = date.getDate();
     const dayValue = (day < 10 ? `0` : ``) + day;
     const dateValue = `${yearValue}-${monthValue}-${dayValue}`;
 
